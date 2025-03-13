0076f3ba2c8511a41b7b554c686259c6cc7326a8
Fatemeh Parsaei ( Mahya ) <persiangirl159@gmail.com>
2025-03-13 10:24:57 +0330

    Update

--- WEEK 1/dese/7.sql
+++ WEEK 1/dese/7.sql
@@ -3,7 +3,7 @@ DESE is preparing a report on schools in the Cambridge school district.
 In 7.sql, write a SQL query to find the names of schools (public or charter!) 
 in the Cambridge school district. Keep in mind that Cambridge, the city, 
 contains a few school districts, but DESE is interested in the district 
-whose name is “Cambridge.”
+whose name is â€œCambridge.â€
 
 
 
