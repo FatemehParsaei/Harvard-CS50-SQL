987811c3c60600c8e9cf491866f2719b4a3ecf47
Fatemeh Parsaei ( Mahya ) <persiangirl159@gmail.com>
2025-03-13 09:50:48 +0330

    Add files via upload

---
+++ WEEK 0/4.sql
@@ -0,0 +1,9 @@
+write a SQL query to count how many prints by Hiroshige have English titles
+that refer to the “Eastern Capital”. Hiroshige’s prints were created
+in Japan’s “Edo period,” referencing the eastern capital city of Edo, now Tokyo.
+
+
+
+
+select count(english_title) from views where "artist"='Hiroshige' and english_title like '%Eastern Capital%';
+
---
+++ WEEK 0/6.sql
@@ -0,0 +1,9 @@
+write a SQL query to find the average entropy of prints by Hiroshige,
+rounded to two decimal places. Call the resulting column “Hiroshige Average Entropy”.
+
+
+
+
+
+select ROUND(AVG("entropy"), 2) as "Hiroshige Average Entropy" from views where "artist"='Hiroshige';
+
