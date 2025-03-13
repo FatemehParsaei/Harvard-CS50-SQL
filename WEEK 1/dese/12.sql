ac5b47d946711d6c89680362a7fe08a831a02eee
Fatemeh Parsaei ( Mahya ) <persiangirl159@gmail.com>
2025-03-13 10:22:24 +0330

    Update

--- WEEK 1/dese/12.sql
+++ WEEK 1/dese/12.sql
@@ -1,8 +1,8 @@
 
 A parent asks you for advice on finding the best public school districts in Massachusetts. 
 In 12.sql, write a SQL query to find public school districts with above-average per-pupil 
-expenditures and an above-average percentage of teachers rated “exemplary”. 
-Your query should return the districts’ names, along with their per-pupil expenditures 
+expenditures and an above-average percentage of teachers rated â€œexemplaryâ€. 
+Your query should return the districtsâ€™ names, along with their per-pupil expenditures 
 and percentage of teachers rated exemplary. Sort the results first by 
 the percentage of teachers rated exemplary (high to low), 
 then by the per-pupil expenditure (high to low).
