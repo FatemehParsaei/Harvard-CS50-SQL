5039f0804f69cc371d84a822947c0026c4307136
Fatemeh Parsaei ( Mahya ) <persiangirl159@gmail.com>
2025-03-13 09:51:52 +0330

    Update

--- WEEK 0/2.sql
+++
@@ -1,8 +0,0 @@
-write a SQL query to list the average colors of prints by Hokusai that
-include “river” in the English title. (As an aside, do they have any hint of blue?)
-
-
-
-
-select average_color from views where "artist"='Hokusai' and "english_title" like '%river%';
-
