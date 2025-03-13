24d393d5853e0593cbd86c540a437a62a25c9990
Fatemeh Parsaei ( Mahya ) <persiangirl159@gmail.com>
2025-03-13 09:53:45 +0330

    Update

--- WEEK 0/views/7.sql
+++ WEEK 0/views/7.sql
@@ -1,6 +1,6 @@
 write a SQL query to list the English titles of the 5 brightest prints by Hiroshige, 
 from most to least bright. Compare them to this list on Wikipedia to see if your
-results match the print’s aesthetics.
+results match the printâ€™s aesthetics.
 
 
 
